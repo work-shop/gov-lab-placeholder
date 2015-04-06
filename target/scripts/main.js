02a4cbe0f028029fcad53ffcf4793eaa88133e29
Nic Schumann <nicolas_schumann@brown.edu>
2015-04-06 14:02:39 -0400

    compiled target files

--- target/scripts/main.js
+++ target/scripts/main.js
@@ -209,6 +209,8 @@ For e.g. 'http://blahblah.us1.list-manage.com/subscribe/post-json?u=5afsdhfuhdsi
 })(jQuery);
 ;(function($) {
 	$(document).ready(function () {
+		var block = $('.block');
+
 		/* 
 			There are two scroll-triggered functions we need, especially.
 
@@ -219,55 +221,84 @@ For e.g. 'http://blahblah.us1.list-manage.com/subscribe/post-json?u=5afsdhfuhdsi
 		*/
 		var action_map = {
 			'#landing-block': [
-				{forward: gray_trigger},
-				{ backward: fade_in }
+				gray_trigger,
+				fade_in
 			],
 			'#about-block': [
-				{forward: brand_trigger},
-				{forward: fade_out, backward: fade_in }
+				brand_trigger,
+				fade_out,
+				fade_in
 			],
 			'#process-block-1': [
-				{forward: dark_gray_trigger},
-				{forward: fade_out, backward: fade_in }
+				dark_gray_trigger,
+				fade_out,
+				fade_in
 			],
 			'#process-block-2': [
-				{forward: gray_trigger}
+				gray_trigger
 			],
 			'#process-block-3': [
-				{forward: dark_gray_trigger},
-				{forward: fade_out, backward: fade_in }
+				dark_gray_trigger,
+				fade_out,
+				fade_in
 			],
 			'#projects-block': [
-				{forward: dark_gray_trigger}, 
-				{forward: fade_out, backward: fade_in }
+				dark_gray_trigger,
+				fade_out,
+				fade_in
 			],
 			'#contact-block': [
-				{forward: dark_gray_trigger},
-				{forward: act_trigger_inverse},
-				{forward: fade_out, backward: fade_in }
+				dark_gray_trigger,
+				act_trigger_inverse,
+				fade_out,
+				fade_in
 			],
 			'#bot': [
-				{forward: act_trigger }
+				act_trigger
 			]
 
-		}
+		};
 
 
 		/** [1]. arrow target swap machine */
 		/** [2]. animation trigger */
 		$(window).on('scroll', function() {
-			update_anchor_target( $('.block:below-the-fold' ), $('#arrow-icon') );
-			do_block_action( $('.block:in-viewport'), action_map );
+
+			update_anchor_target( select_next_block( block ), $('#arrow-icon') );
+
+			do_block_action( $('.block, #bot').filter(':in-viewport'), action_map );
+
 		});
 
-		$(window).on('resize', function() {
-			update_anchor_target( $('.block:below-the-fold' ), $('#arrow-icon') );
-			do_block_action( $('.block:in-viewport'), action_map );
-		});
+		$(document).on('dom-is-sized', function() {
 
-		update_anchor_target( $('.block:below-the-fold' ), $('#down-arrow') );
+			update_anchor_target( select_next_block( block ), $('#down-arrow') );
+
+		});
 	});
 })( jQuery );
+
+/**
+ * This routine reduces the set of potential arrow-targets to
+ * the next block;
+ *
+ */
+function select_next_block( block ) {
+	var currentview = block.filter(':in-viewport')
+	  , nextview =  block.filter(':below-the-fold');
+
+	if ( currentview.length == 1 ) { return nextview.first(); }
+
+	else if ( currentview.length > 1 ) { return currentview.last(); }
+
+	else {
+		
+		console.log('no blocks!');
+		return false;
+	}
+}
+
+/** Arrow Color Change Functions */
 
 function brand_trigger( current ){
 	$('#arrow-icon').removeClass('gray').removeClass('dark-gray').addClass('brand');
@@ -281,15 +312,25 @@ function dark_gray_trigger( current ){
 	$('#arrow-icon').removeClass('brand').removeClass('gray').addClass('dark-gray');
 }
 
+/** Arrow Animation Trigger */
+
 function act_trigger( current ) {
-	if ( !($('#down-arrow').hasClass( 'animation-active')) )  {
-		$('#down-arrow' ).addClass('animation-active');
-	}
+	/* Not Cursor Pointer */
+
+	$('#down-arrow').fadeOut(300);
+
+	// if ( !($('#down-arrow').hasClass( 'animation-active')) )  {
+	// 	$('#down-arrow' ).addClass( 'animation-active');
+	// }
 }
 
 function act_trigger_inverse( current ) {
-	$('#down-arrow').removeClass('animation-active');
+
+	$('#down-arrow').fadeIn(400);
+	//$('#down-arrow').removeClass('animation-active');
 } 
+
+/** Section Introduction Text Cue */
 
 function fade_in( current ) {
 	$('#' + current.last().prev('section').attr('id') + '-cue' ).fadeIn();
@@ -297,50 +338,58 @@ function fade_in( current ) {
 }
 
 function fade_out( current ) {
-	console.log( $('#' + current.last().attr('id') + '-cue' ) );
+	//console.log( $('#' + current.last().attr('id') + '-cue' ) );
 	$('#' + current.last().attr('id') + '-cue' ).fadeOut();
 }
+
+
 
 /** [1]. arrow target swap machine - callback */
 
 function update_anchor_target( next, selector ) {
+	
 	selector.off();
-	selector.on('click', target_handler( next ) );
+	selector.one('click', target_handler( next ) );
 }
 
-function target_handler( target, selector ) {
+function target_handler( target ) {
 	return function() {
+		console.log( "target parameter to 'target_handler':");
+		console.log( target );
+
 		if ( target.length > 0 ) {
-			//selector.css({'z-index': 1000});
 			$('html, body').animate({
 				scrollTop: target.offset().top
 			}, 1500);
-		} else {
-			selector.off('click');
-			selector.css({'z-index': 500});
-			// $('html, body').animate({
-			// 	scrollTop: 0
-			// }, 1500);
-		}	
+		} 
 	};
 }
+
+
 
 
 /** [2]. animation trigger - callback */
 
 function do_block_action( block, actions ) {
+	block = block.last();
+
+	console.log( '\n\n current block.last() :::');
 	console.log( block );
-	var id = '#' + block.last().attr('id');
+
+	var id = '#' + block.attr('id');
+
 	if ( id && actions[ id ] ) {
+
 		if ( actions[id].length ) {
-			actions[id].forEach(function( action ) {
-				if (action.forward) action.forward( block );
-				if (action.backward) action.backward( block );
-			});
+
+			actions[id].forEach( function( action ) { action( block ); } );
+
 		} else {
-			if (actions[id].forward) actions[id].forward( block );
-			if (actions[id].backward) actions[id].backward( block );
+
+			 actions[id]( block );
+
 		}
+
 	}
 }
 
@@ -378,6 +427,8 @@ function equal_width( target, selector ) {
 				); 
 			}	
 		}
+
+		$(document).trigger('dom-is-sized');
 	}
 
 	var actionmap = 
